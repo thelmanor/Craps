413aed5f6b2bdba04d0ccc559096dc3507c5ef6a
thelma <Thelmanor@gmail.com>
2019-03-05 14:28:20 -0500

    Craps

--- Craps/Form1.Designer.cs
+++ Craps/Form1.Designer.cs
@@ -28,13 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.roll_btn = new System.Windows.Forms.Button();
+            this.play_btn = new System.Windows.Forms.Button();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.die1img = new System.Windows.Forms.PictureBox();
+            this.die2img = new System.Windows.Forms.PictureBox();
+            this.die3img = new System.Windows.Forms.PictureBox();
+            this.die4img = new System.Windows.Forms.PictureBox();
+            this.die5img = new System.Windows.Forms.PictureBox();
+            this.die6img = new System.Windows.Forms.PictureBox();
+            this.die1 = new System.Windows.Forms.PictureBox();
+            this.die2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.die1img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die2img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die3img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die4img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die5img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die6img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // roll_btn
+            // 
+            this.roll_btn.Location = new System.Drawing.Point(144, 124);
+            this.roll_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.roll_btn.Name = "roll_btn";
+            this.roll_btn.Size = new System.Drawing.Size(113, 19);
+            this.roll_btn.TabIndex = 2;
+            this.roll_btn.Text = "Roll Dice";
+            this.roll_btn.UseVisualStyleBackColor = true;
+            this.roll_btn.Click += new System.EventHandler(this.roll_btn_Click);
+            // 
+            // play_btn
+            // 
+            this.play_btn.Location = new System.Drawing.Point(295, 124);
+            this.play_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.play_btn.Name = "play_btn";
+            this.play_btn.Size = new System.Drawing.Size(89, 19);
+            this.play_btn.TabIndex = 3;
+            this.play_btn.Text = "Play Again";
+            this.play_btn.UseVisualStyleBackColor = true;
+            this.play_btn.Click += new System.EventHandler(this.play_btn_Click);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Location = new System.Drawing.Point(243, 171);
+            this.exit_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(56, 19);
+            this.exit_btn.TabIndex = 4;
+            this.exit_btn.Text = "Exit";
+            this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // die1img
+            // 
+            this.die1img.Image = ((System.Drawing.Image)(resources.GetObject("die1img.Image")));
+            this.die1img.Location = new System.Drawing.Point(11, 10);
+            this.die1img.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die1img.Name = "die1img";
+            this.die1img.Size = new System.Drawing.Size(77, 81);
+            this.die1img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.die1img.TabIndex = 5;
+            this.die1img.TabStop = false;
+            this.die1img.Visible = false;
+            // 
+            // die2img
+            // 
+            this.die2img.Image = ((System.Drawing.Image)(resources.GetObject("die2img.Image")));
+            this.die2img.Location = new System.Drawing.Point(100, 10);
+            this.die2img.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die2img.Name = "die2img";
+            this.die2img.Size = new System.Drawing.Size(75, 81);
+            this.die2img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.die2img.TabIndex = 6;
+            this.die2img.TabStop = false;
+            this.die2img.Visible = false;
+            // 
+            // die3img
+            // 
+            this.die3img.Image = ((System.Drawing.Image)(resources.GetObject("die3img.Image")));
+            this.die3img.Location = new System.Drawing.Point(194, 10);
+            this.die3img.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die3img.Name = "die3img";
+            this.die3img.Size = new System.Drawing.Size(75, 81);
+            this.die3img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.die3img.TabIndex = 7;
+            this.die3img.TabStop = false;
+            this.die3img.Visible = false;
+            // 
+            // die4img
+            // 
+            this.die4img.Image = ((System.Drawing.Image)(resources.GetObject("die4img.Image")));
+            this.die4img.Location = new System.Drawing.Point(286, 10);
+            this.die4img.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die4img.Name = "die4img";
+            this.die4img.Size = new System.Drawing.Size(75, 81);
+            this.die4img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.die4img.TabIndex = 8;
+            this.die4img.TabStop = false;
+            this.die4img.Visible = false;
+            // 
+            // die5img
+            // 
+            this.die5img.Image = ((System.Drawing.Image)(resources.GetObject("die5img.Image")));
+            this.die5img.Location = new System.Drawing.Point(381, 10);
+            this.die5img.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die5img.Name = "die5img";
+            this.die5img.Size = new System.Drawing.Size(75, 81);
+            this.die5img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.die5img.TabIndex = 9;
+            this.die5img.TabStop = false;
+            this.die5img.Visible = false;
+            // 
+            // die6img
+            // 
+            this.die6img.Image = ((System.Drawing.Image)(resources.GetObject("die6img.Image")));
+            this.die6img.Location = new System.Drawing.Point(482, 10);
+            this.die6img.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die6img.Name = "die6img";
+            this.die6img.Size = new System.Drawing.Size(75, 81);
+            this.die6img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.die6img.TabIndex = 10;
+            this.die6img.TabStop = false;
+            this.die6img.Visible = false;
+            // 
+            // die1
+            // 
+            this.die1.Location = new System.Drawing.Point(113, 235);
+            this.die1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die1.Name = "die1";
+            this.die1.Size = new System.Drawing.Size(75, 68);
+            this.die1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.die1.TabIndex = 11;
+            this.die1.TabStop = false;
+            // 
+            // die2
+            // 
+            this.die2.Location = new System.Drawing.Point(321, 235);
+            this.die2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.die2.Name = "die2";
+            this.die2.Size = new System.Drawing.Size(75, 68);
+            this.die2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.die2.TabIndex = 12;
+            this.die2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.die2);
+            this.Controls.Add(this.die1);
+            this.Controls.Add(this.die6img);
+            this.Controls.Add(this.die5img);
+            this.Controls.Add(this.die4img);
+            this.Controls.Add(this.die3img);
+            this.Controls.Add(this.die2img);
+            this.Controls.Add(this.die1img);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.play_btn);
+            this.Controls.Add(this.roll_btn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.die1img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die2img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die3img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die4img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die5img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die6img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.die2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button roll_btn;
+        private System.Windows.Forms.Button play_btn;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.PictureBox die1img;
+        private System.Windows.Forms.PictureBox die2img;
+        private System.Windows.Forms.PictureBox die3img;
+        private System.Windows.Forms.PictureBox die4img;
+        private System.Windows.Forms.PictureBox die5img;
+        private System.Windows.Forms.PictureBox die6img;
+        private System.Windows.Forms.PictureBox die1;
+        private System.Windows.Forms.PictureBox die2;
     }
 }
 
--- Craps/Form1.cs
+++ Craps/Form1.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**********************************
+ * Thelma Nora
+ * CIST 2341
+ * Mini-Project (Craps)
+ * 11/25/2018
+ * ******************************/
+
 namespace Craps
 {
     public partial class Form1 : Form
@@ -15,6 +22,167 @@ namespace Craps
         public Form1()
         {
             InitializeComponent();
+        }
+        int dice1 = 0;
+        int dice2 = 0;
+        int totalscore;
+        int pointnum;
+        int game1 = 0;
+        int count=0;
+        Random rn = new Random();
+        private void roll_btn_Click(object sender, EventArgs e)
+        {
+			PlayCraps();
+        }
+		
+        //Method for generating the random numbers  and displaying the dice on the GUI
+        public int Roll()
+        {
+            dice1 = rn.Next(1, 7);
+            Console.WriteLine(dice1);
+            dice2 = rn.Next(1, 7);
+            Console.WriteLine(dice2);
+
+            if (dice1 == 1)
+            {
+                die1.Image = die1img.Image;
+            }
+            else if (dice1 == 2)
+            {
+                die1.Image = die2img.Image;
+
+            }
+            else if (dice1 == 3)
+            {
+                die1.Image = die3img.Image;
+            }
+            else if (dice1 == 4)
+            {
+                die1.Image = die4img.Image;
+            }
+            else if (dice1 == 5)
+            {
+                die1.Image = die5img.Image;
+            }
+            else if (dice1 == 6)
+            {
+                die1.Image = die6img.Image;
+            }
+
+            if (dice2 == 1)
+            {
+                die2.Image = die1img.Image;
+            }
+            else if (dice2 == 2)
+            {
+                die2.Image = die2img.Image;
+            }
+            else if (dice2 == 3)
+            {
+                die2.Image = die3img.Image;
+            }
+            else if (dice2 == 4)
+            {
+                die2.Image = die4img.Image;
+            }
+            else if (dice2 == 5)
+            {
+                die2.Image = die5img.Image;
+            }
+            else if (dice2 == 6)
+            {
+                die2.Image = die6img.Image;
+            }
+
+            totalscore = dice1 + dice2;
+            Console.WriteLine("Player rolled:" + totalscore);
+            return totalscore;
+        }
+
+        // Method for playing the game.
+       public  void PlayCraps()
+        {
+           int firstroll= Roll();   
+            // do-while loop that starts the game  to determine if player rolled a winning or lossingnumber on their first try
+            do
+            {
+                //if statement to display winning number on first roll
+                if ((firstroll == 7) || (firstroll== 11))
+                {
+                    Console.WriteLine("Congratulations!!! You Win!");
+                    break;
+
+                }
+                //if statement to display losing numbers on first roll
+                else if ((firstroll == 2) || (firstroll== 3) || (firstroll== 12))
+                {
+                    Console.WriteLine("Sorry, You Lost!");
+                    break;
+                }
+                //else statement to roll again if the first roll doesn't generate a winning or lossing number 
+                else
+                {
+                    //setting variable for the point number
+                    pointnum = firstroll;
+                    int newroll = Roll();
+                    //if statement to capture possible win on second roll 
+                    if (newroll == pointnum)
+                        {
+                        Console.WriteLine("The point number is:" + pointnum);
+                        Console.WriteLine("second roll:Congratulations!!! You Win!");
+                        break;
+                        }
+                    //if statement to capture possible loss on second roll
+                    else if (newroll == 7)
+                        {
+                        Console.WriteLine("The point number is:" + pointnum +"you have rolled a 7");
+                        Console.WriteLine("Sorry, You Loss!");
+                        break;
+                        }
+                    //statement to ensure that player keeps rolling if the second roll doesnt match the point number of lossing number
+                    if(newroll!=pointnum || newroll!=7){
+                            int bonus= Roll();
+                        //statement that displays when player finally gets a number matching the point number
+                            if (bonus == pointnum)
+                            {
+                                Console.WriteLine("The point number is:" + pointnum);
+                                Console.WriteLine("bonus rolls:Congratulations!!! You Win!");
+                                break;
+                            }
+                        //statement that displays when player finally gets a 7 and losses the game
+                        if (bonus == 7)
+                            {
+                                Console.WriteLine("The point number is:" + pointnum);
+                                Console.WriteLine("bonus rolls:You loss!!");
+                                break;
+                            }
+                        count++;   
+                    }     
+                }
+            }
+            while (game1<1);            
+        }
+
+        private void play_btn_Click(object sender, EventArgs e)
+        {
+
+            DialogResult answer = MessageBox.Show("Would you like to play again?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (answer == DialogResult.No)
+            {
+                Application.Exit();
+            }
+            if (answer == DialogResult.Yes)
+            {
+                die1.Image = null;
+                die2.Image = null;
+                Console.WriteLine("Roll dice to play again");
+            }
+
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
